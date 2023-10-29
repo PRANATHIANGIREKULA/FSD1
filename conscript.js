04f534ac2c76c296e381270218a86dbcaf17e490
PRANATHI.ANGIREKULA <118327715+PRANATHIANGIREKULA@users.noreply.github.com>
2023-10-29 17:50:01 +0530

    Add files via upload

---
+++ conscript.js
@@ -0,0 +1,27 @@
+scriptcon.js
+
+const messageForm = document.getElementById("messageForm");
+
+messageForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    // Here, you can send the message data to a server for further processing
+
+    // For demonstration, show an alert with the submitted information
+    console.log('Name: ${name}');
+    console.log('Email: ${email}');
+    console.log('Message: ${message}');
+    alert('Message Sent:\nName: ${name}\nEmail: ${email}\nMessage: ${message}');
+    
+    // Clear the form fields after submission
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+});
+
+
+
